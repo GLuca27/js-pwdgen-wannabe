e92d5ecc9b6e0fad29f4c3dff8d287c9dca303f6
GLuca27 <gianluca.sciglitano1@gmail.com>
2022-10-19 17:18:56 +0200

    ticket

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const firstName = prompt ("Inserisci il tuo nome");
+console.log(firstName);
+
+const lastName = prompt ("Inserisci il tuo cognome");
+console.log(lastName);
+
+const color = prompt ("Inserisci il tuo colore preferito");
+console.log(color);
+
+const password = $(firstName lastName color) 76;
+console.log(password);
+
+document.querySelector("h1").innerHTML = password;
