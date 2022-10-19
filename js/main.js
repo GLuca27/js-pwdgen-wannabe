dbad71b5f34c38ab2c747d4d966c86d258d6e325
GLuca27 <gianluca.sciglitano1@gmail.com>
2022-10-19 17:31:06 +0200

    finito

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ console.log(lastName);
 const color = prompt ("Inserisci il tuo colore preferito");
 console.log(color);
 
-const password = $(firstName lastName color) 76;
+const password = `${firstName}${lastName}${color}76`;
 console.log(password);
 
 document.querySelector("h1").innerHTML = password;
